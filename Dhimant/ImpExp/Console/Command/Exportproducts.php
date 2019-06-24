8b3f6ab7c7036c8a3b945cd315867e200a6dbe5a
Dhimant <noobprogrammer86@gmail.com>
2019-06-25 00:04:17 +0530

    Initialization

---
+++ Dhimant/ImpExp/Console/Command/Exportproducts.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) 2019  
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Dhimant\ImpExp\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Exportproducts extends Command
+{
+
+    const NAME_ARGUMENT = "name";
+    const NAME_OPTION = "option";
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $name = $input->getArgument(self::NAME_ARGUMENT);
+        $option = $input->getOption(self::NAME_OPTION);
+        $output->writeln("Hello " . $name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName("dhimant_impexp:exportproducts");
+        $this->setDescription("Export Products to CSV");
+        $this->setDefinition([
+            new InputArgument(self::NAME_ARGUMENT, InputArgument::OPTIONAL, "Name"),
+            new InputOption(self::NAME_OPTION, "-a", InputOption::VALUE_NONE, "Option functionality")
+        ]);
+        parent::configure();
+    }
+}
